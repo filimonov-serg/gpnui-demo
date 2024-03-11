1e8117a03035c42fecdbceb9ab02310b76ffe168
Filimonov Sergey <filimonov.serg@gmail.com>
2024-03-11 08:56:53 +0300

    fix styles

--- assets/Main-CV0mo8LP.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./lim_plugin-vuelim_export-helper-DlAUqK2U.js";import{o as c,c as o}from"./index-CGZQSZF1.js";const r={};function n(t,a){return c(),o("div",null,"123")}const f=e(r,[["render",n]]);export{f as default};
