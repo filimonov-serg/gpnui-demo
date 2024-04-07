52c34d6ea11e57a2e3f149a322a8fe818a02836e
Filimonov Sergey <filimonov.serg@gmail.com>
2024-04-07 16:39:03 +0300

    new version

---
+++ assets/Main-CrrZjvcV.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./lim_plugin-vuelim_export-helper-DlAUqK2U.js";import{o as c,c as o}from"./index-BuqtH1Xl.js";const r={};function n(t,a){return c(),o("div",null,"123")}const f=e(r,[["render",n]]);export{f as default};
+//# sourceMappingURL=Main-CrrZjvcV.js.map
